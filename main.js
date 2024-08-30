1705dd25d510f711a6a49790fbd9d07ef60db64a
EduardoSa23 <eduardo.sa2398@gmail.com>
2024-08-29 21:01:00 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,42 @@
+const form = document.getElementById('form-atividade');
+const nome = [];
+const telefones = [];
+
+form.addEventListener('submit' , function(e){
+    e.preventDefault();
+    adicionarLinha();
+})
+
+function adicionarLinha() {
+    const nome = document.getElementById('nome').value;
+    const telefone = document.getElementById('telefone').value;
+
+    const telefoneExistente = Array.from(document.querySelectorAll('#lista-contatos td:nth-child(2)'))
+                                .map(td => td.textContent);
+    
+    if (telefoneExistente.includes(telefone)) {
+        alert('Este número de telefone já está cadastrado.');
+        document.getElementById('nome').value = '';
+        document.getElementById('telefone').value = '';
+        return;
+    }
+    
+    const tbody = document.getElementById('lista-contatos');
+    const tr = document.createElement('tr');
+    tr.innerHTML = `
+        <td>${nome}</td>
+        <td>${telefone}</td>
+        <td><button class="delete">Excluir</button></td>
+    `;
+
+    tbody.appendChild(tr);
+
+    
+
+    document.getElementById('nome').value = '';
+    document.getElementById('telefone').value = '';
+
+    tr.querySelector('.delete').addEventListener('click', function() {
+        tr.remove();
+    });
+}
