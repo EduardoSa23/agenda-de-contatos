45ed12795bb6d0c7dca9d77f1196148b29c3b5ca
EduardoSa23 <eduardo.sa2398@gmail.com>
2024-09-01 08:46:41 -0300

    removendo o bug do botao excluir

--- main.js
+++ main.js
@@ -26,7 +26,7 @@ function adicionarLinha() {
     tr.innerHTML = `
         <td>${nome}</td>
         <td>${telefone}</td>
-        <td><button class="call"><a href="tel:${telefone}"</a><i class="fas fa-phone"></i></button><button class="delete">Excluir</button></td>
+        <td></button><button class="delete">Excluir</button><button class="call"><a href="tel:${telefone}"</a><i class="fas fa-phone"></i></td>
     `;
 
     tbody.appendChild(tr);
